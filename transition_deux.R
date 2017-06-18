683c8de5d7a1c239895ca6a722ab0840003e1a7f
alleznoobs68 <216159@supinfo.com>
2017-06-18 12:00:38 +0200

    Add files via upload

--- transition_deux.R
+++ transition_deux.R
@@ -1,10 +1,11 @@
 trajectoireMatrice<-function(matrice, etat, n)
 {
-  i <- 1      #Definition d'une variable nul pour la boucle while pour le temps n
-  y <- 1      #Definition d'une variable nul pour la boucle while pour le parcours d'une ligne
+  i <- 2      #Definition d'une variable pour la boucle while pour le temps n
   trajectoire <- vector("numeric",length = n)    #Création d'un vecteur de longueur n de variables nuls
+  trajectoire[1] <- etat
   while(i <= n)
   {
+    y <- 1      #Definition d'une variable nul pour la boucle while pour le parcours d'une ligne
     changement <- FALSE  #Definition d'un booleen pour le changement d'état
     probabilite <- sample(1:10, 1)        #Nombre aléatoire entre 1 et 10
     longueur <- length(matrice[etat,])    #Longueur de la ligne
@@ -13,8 +14,11 @@ trajectoireMatrice<-function(matrice, etat, n)
     {
       if(changement == FALSE)
       {
-        intervalle <- seq(variable,matrice[etat,y]*10)
-        variable <- matrice[etat,y]*10
+        intervalle <- seq(variable,variable+matrice[etat,y]*10)
+        if(matrice[etat,y]*10 != 0)
+        {
+          variable <- matrice[etat,y]*10
+        }
         if(probabilite %in% intervalle)
         {
           trajectoire[i] <- y
