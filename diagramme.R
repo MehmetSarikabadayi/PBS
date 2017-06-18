68bb70c3b2496794389668a2fb157bb91f530dc9
alleznoobs68 <216159@supinfo.com>
2017-06-18 20:56:19 +0200

    Add files via upload

--- diagramme.R
+++ diagramme.R
@@ -1,8 +1,4 @@
-Diagramme <- function(chaine){
-  print(chaine)
-  
-  
-  barplot(chaine,main = " diagramme bâton des fréquences des états occupés lors de la trajectoire",xlab = "état",ylab = "trajectoire",xlim = c(0,20),col = c(1,2,3))
+Diagramme <- function(Trajectoire){
+  plot(table(Trajectoire),yaxt="n",main="Diagramme bâton des fréquences des états occupés")
   box()
-  legend(x="topright",legend = c("Etat1","Etat 2","Etat 3"),fill = c(1,2,3))
 }
