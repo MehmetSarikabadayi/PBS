870eb69ae025a1282ae1255f87679e2333e4f0f7
alleznoobs68 <216159@supinfo.com>
2017-06-18 12:34:55 +0200

    Add files via upload

--- transition.R
+++ transition.R
@@ -1,13 +1,13 @@
 trajectoireMatrice<-function(matrice, etat, n)
 {
   i <- 2      #Definition d'une variable pour la boucle while pour le temps n
-  trajectoire <- rep(3,n)   #Création d'un vecteur de longueur n de variables nuls
+  trajectoire <- rep(3,n)   #Crï¿½ation d'un vecteur de longueur n
   trajectoire[1] <- etat
   while(i <= n)
   {
     y <- 1      #Definition d'une variable pour la boucle while pour le parcours d'une ligne
-    changement <- FALSE  #Definition d'un booleen pour le changement d'état
-    probabilite <- sample(1:10, 1)        #Nombre aléatoire entre 1 et 10
+    changement <- FALSE  #Definition d'un booleen pour le changement d'ï¿½tat
+    probabilite <- sample(1:10, 1)        #Nombre alï¿½atoire entre 1 et 10
     longueur <- length(matrice[etat,])    #Longueur de la ligne
     variable <- 0
     while(y <= longueur)
